8e17548e7ad97be9b4ba9320b47e7538a99fb652
tomka2n <tomka2n@users.noreply.github.com>
2026-04-01 12:30:28 +0000

    feat: add zsh completion for arduino-cli in setup script

--- .devcontainer/scripts/init_arduino-cli.sh
+++ .devcontainer/scripts/init_arduino-cli.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CONFIG_PATH="/home/vscode/.arduino15/arduino-cli.yaml"
+ZSH_COMP_DIR="/home/vscode/.zsh/completion"
 
 if [ ! -f "$CONFIG_PATH" ]; then
     echo "[Init] Creating initial Arduino CLI config..."
@@ -9,10 +10,16 @@ if [ ! -f "$CONFIG_PATH" ]; then
     arduino-cli config set directories.user /workspace
     arduino-cli config set build_cache.path /home/vscode/.arduino15/BUILD_CACHE
 
-#    arduino-cli config add board_manager.additional_urls \
-#      https://espressif.github.io/arduino-esp32/package_esp32_index.json
-
     echo "[Init] Arduino CLI config generated."
 else
     echo "[Init] arduino-cli.yaml already exists. Skipping init."
+fi
+
+echo "[Init] Setting up zsh autocompletion..."
+mkdir -p "$ZSH_COMP_DIR"
+arduino-cli completion zsh > "$ZSH_COMP_DIR/_arduino-cli"
+
+if ! grep -q "fpath=(~/.zsh/completion \$fpath)" ~/.zshrc; then
+    echo 'fpath=(~/.zsh/completion $fpath)' >> ~/.zshrc
+    echo 'autoload -Uz compinit && compinit' >> ~/.zshrc
 fi
